9b09463730d5798fb495dd06e0de79d70cec4158
Jeffrey Licht <jeffrey.licht@gmail.com>
2014-01-17 10:07:01 -0500

    Added balatarin (english)

--- js/cayl.js
+++ js/cayl.js
@@ -1,39 +1,38 @@
 var message = "Cached";      
-var link = "http://www.youtube.com/watch?v=oHg5SJYRHA0";
+var link = "http://www.youtube.com/watch?v=oHg5SJYRHA0";               
 
 if (window.location.search) {
   var q = window.location.search.substring(1);
-  console.log(q);
   switch (q) {
     case "hover" :
-      $(function() {
-        $("a").each(function(i,e) {
+      jQuery( document ).ready(function( $ ) {
+        $("a[href^=http]").not("[href*=" + caylExclude + "]").each(function(i,e) {
           $(this).after("<span class='cayl'><a href='" + link + "'>" + message + "</a></span>");
         })
       });
       break;        
                 
     case "hover2":
-      $(function() {
-        $("a").each(function(i,e) {
+      jQuery( document ).ready(function( $ ) {
+        $("a[href^=http]").not("[href*=" + caylExclude + "]").each(function(i,e) {
           $(this).after("<span class='cayl'><a href='" + link + "'>" + message + "</a></span>");
         })                                  
-        $("a").hoverIntent({ over: function() { $(this).next("span.cayl").addClass("on"); }, 
+        $("a[href^=http]").not("[href*=" + caylExclude + "]").hoverIntent({ over: function() { $(this).next("span.cayl").addClass("on"); }, 
                              out: function() { $(this).next("span.cayl").removeClass("on"); },
                              interval: 500}  )
       });  
       break;
 
     case "insert":
-      $(function(){   
-        $("a").click(function() {
+      jQuery( document ).ready(function( $ ) {   
+        $("a[href^=http]").not("[href*=" + caylExclude + "]").click(function() {
           $(this).after("<span class='cayl'><a href='" + link + "'>" + message + "</a></span>");
         })          
       });
       break;
       
     case "menu":
-      $(function(){   
+      jQuery( document ).ready(function( $ ) {   
         $.contextMenu({
           selector: "a",            
           callback: function(key, options) {
@@ -45,17 +44,17 @@ if (window.location.search) {
       break;
       
       case "state":
-        function banner(event) {     
+        function banner(event) {  
           if (event.originalEvent.state.link && event.originalEvent.state.target) {
-            $("body").append("<div class='cayl-banner'>Problems viewing " + 
+            jQuery("body").append("<div class='cayl-banner'>Problems viewing " + 
                   event.originalEvent.state.target + "? <a href='" + event.originalEvent.state.link + 
                   "'>Cached version here</a>");
-            window.setTimeout(function() { $(".cayl-banner").fadeOut(2000);}, 5000);
+            window.setTimeout(function() { jQuery(".cayl-banner").fadeOut(2000);}, 5000);
           }
         }                           
 
-        $(function(){                  
-          $("a").click(function() {
+        jQuery( document ).ready(function( $ ) {                  
+          $("a[href^=http]").not("[href*=" + caylExclude + "]").click(function() {
             history.pushState({link: link, target: $(this).attr("href")},"");    
             history.pushState({},"","#cayl");    
           });                                
@@ -65,8 +64,8 @@ if (window.location.search) {
         break;
         
       case "widget":
-        $(function() {
-          $("a").each(function(i,e) {
+        jQuery( document ).ready(function( $ ) {
+          $("a[href^=http]").not("[href*=" + caylExclude + "]").each(function(i,e) {
             $(this).after("<span class='cayl-widget'><span class='cayl'><a href='" + link + "'>" + message + "</a></span>");
           })             
           $("span.cayl-widget").click(function(e) {$(this).children().toggleClass("on"); e.stopPropagation(); });
