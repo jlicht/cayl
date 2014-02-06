4194d220c8edd214829e73ea252ca3bcabde4bc3
Jeffrey Licht <jeffrey.licht@gmail.com>
2014-02-06 12:26:18 -0500

    Started translating into Farsi

--- prototype/js/cayl.js
+++ prototype/js/cayl.js
@@ -1,7 +1,29 @@
 var cayl = {
 
   hovering_on_popup : false,
-
+  translations : {
+    en : {
+      interstitial_html : 
+      '<div class="cayl-interstitial"><a href="#" class="cayl-close"></a><div class="cayl-body"><div class="cayl-message"><span>The link you clicked may not be available for viewing.</span><br/>We did find a cached version of this page.</div>' +
+      '<div class="cayl-cached"><div>The live link may not lead to the page you are looking for and may have been replaced or routed to another server.<div>Our cached link is from<br/>  ' +
+      '{{DATE}}</div></div><a href="{{CACHE}}">View the cached link</a></div><div class="cayl-live"><div><iframe src="{{LINK}}"/></div><a href="{{LINK}}">View the live link</a></div>' +
+      '<div class="cayl-credit">Balatarin uses <a href="#">CAYL</a></div></div></div>',
+      hover_html_up : '<div class="cayl-hover cayl-up"><div class="cayl-text">This site should be available</div><a href="{{LINK}}">View the Live Link</a><a href="{{CACHE}}">View the Cached Link</a><div class="cayl-arrow"></div></div>',
+      hover_html_down : '<div class="cayl-hover cayl-down"><div class="cayl-text">This site may not be available</div><a href="{{LINK}}" class="cayl-live">View the Live Link</a><a href="{{CACHE}}" class="cayl-cache">View the Cached Link</a><div class="cayl-credit">Balatarin uses <a href="#">CAYL</a></div><div class="cayl-arrow"></div></div>',      
+    },
+    fa : {
+        interstitial_html : 
+        '<div class="cayl-interstitial"><a href="#" class="cayl-close"></a><div class="cayl-body"><div class="cayl-message"><span>لینکی که کلیک کردید ممکن است در دسترس برای مشاهده شود.</span><br/>ما پیدا کردن یک نسخه ذخیره سازی این صفحه.</div>' +
+        '<div class="cayl-cached"><div>پیوند زندگی می کنند ممکن است به صفحه شما به دنبال نمی شود و ممکن است جایگزین شده است و یا روت به سرور دیگری.<div>اتصال به مخزن ما این است که از<br/>  ' +
+        '{{DATE}}</div></div><a href="{{CACHE}}">نمایش لینک های cache شده</a></div><div class="cayl-live"><div><iframe src="{{LINK}}"/></div><a href="{{LINK}}">نمایش لینک های فعال</a></div>' +
+        '<div class="cayl-credit">بالاترین از <a href="#"> CAYL </ A></div></div></div>',
+        hover_html_up : '<div class="cayl-hover cayl-up"><div class="cayl-text">This site should be available</div><a href="{{LINK}}">View the Live Link</a><a href="{{CACHE}}">View the Cached Link</a><div class="cayl-arrow"></div></div>',
+        hover_html_down : '<div class="cayl-hover cayl-down"><div class="cayl-text">This site may not be available</div><a href="{{LINK}}" class="cayl-live">View the Live Link</a><a href="{{CACHE}}" class="cayl-cache">View the Cached Link</a><div class="cayl-credit">Balatarin uses <a href="#">CAYL</a></div><div class="cayl-arrow"></div></div>',      
+      },      
+    },
+  
+  text : {},
+                     
   replace_args : function (s, args) {
     for (var key in args) {
       s = s.replace(new RegExp(key,"g"), args[key]);
@@ -10,11 +32,6 @@ var cayl = {
   },
 
   show_interstitial : function (e) {                    
-    var interstitial_html = 
-    '<div class="cayl-interstitial"><a href="#" class="cayl-close"></a><div class="cayl-body"><div class="cayl-message"><span>The link you clicked may not be available for viewing.</span><br/>We did find a cached version of this page.</div>' +
-    '<div class="cayl-cached"><div>The live link may not lead to the page you are looking for and may have been replaced or routed to another server.<div>Our cached link is from<br/>  ' +
-    '{{DATE}}</div></div><a href="{{CACHE}}">View the cached link</a></div><div class="cayl-live"><div><iframe src="{{LINK}}"/></div><a href="{{LINK}}">View the live link</a></div>' +
-    '<div class="cayl-credit">Balatarin uses <a href="#">CAYL</a></div></div></div>';
                                                  
     /* Add the window to the DOM */                                                                     
     jQuery("body").append('<div class="cayl-overlay"></div>');
@@ -26,7 +43,7 @@ var cayl = {
       '{{CACHE}}' : jQuery(this).attr("data-ircache-location"),
     }                                                
 
-    jQuery("body").append(cayl.replace_args(interstitial_html,replacements));    
+    jQuery("body").append(cayl.replace_args(cayl.text.interstitial_html,replacements));    
 
     /* Center the window */
     var left = jQuery(window).width()/2 - jQuery(".cayl-interstitial").width()/2;
@@ -60,9 +77,7 @@ var cayl = {
   },
 
   start_link_hover : function (e) {               
-    var hover_html_up = '<div class="cayl-hover cayl-up"><div class="cayl-text">This site should be available</div><a href="{{LINK}}">View the Live Link</a><a href="{{CACHE}}">View the Cached Link</a><div class="cayl-arrow"></div></div>';
-    var hover_html_down = '<div class="cayl-hover cayl-down"><div class="cayl-text">This site may not be available</div><a href="{{LINK}}" class="cayl-live">View the Live Link</a><a href="{{CACHE}}" class="cayl-cache">View the Cached Link</a><div class="cayl-credit">Balatarin uses <a href="#">CAYL</a></div><div class="cayl-arrow"></div></div>';
-                                                         
+                                                        
     var status = jQuery(this).attr("data-ircache-status");
     var delay = jQuery(this).attr("data-ircache-hover-delay");
     var args = {
@@ -72,7 +87,7 @@ var cayl = {
     t = this;
     if (delay) {
       var timer = setTimeout(function() {                  
-        jQuery("body").append(cayl.replace_args(status == "up" ? hover_html_up : hover_html_down, args));
+        jQuery("body").append(cayl.replace_args(status == "up" ? cayl.text.hover_html_up : cayl.text.hover_html_down, args));
 
         /* Position the hover text */
         var offset = jQuery(t).offset();
@@ -93,9 +108,9 @@ var cayl = {
       }    
     },100);
   },      
-
 };
-
+          
+cayl.text = cayl.translations.en;
 
 jQuery(document).ready(function($) {    
     $("a[data-ircache-location][data-ircache-behavior=override]").click(cayl.show_interstitial);    
